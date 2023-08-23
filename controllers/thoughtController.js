38059d3c8a212c6f03b366baba3aa79e4033301a
Mikayla <user@users-MBP.attlocal.net>
2023-08-23 10:12:21 -0400

    added delete reaction method for thought controller, refactored await and created userController methods

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -65,4 +65,22 @@ module.exports = {
       res.status(500).json(err)
     }
   },
+  async deleteReaction(req, res){
+    try {
+        
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { _id: req.params.reactionId } } },
+            { runValidators: true, new: true }
+          )
+
+      if (!thought) {
+        return res.status(404).json({ message: 'Not found with this ID!' });
+      }
+
+      res.json(thought);
+    }catch (err){
+        res.status(500).json(err);
+    }
+  },
 };
