87d1a39ff40c79ca76943992c2afd15577ffc560
Mikayla <user@users-MBP.attlocal.net>
2023-08-23 10:00:10 -0400

    schemas and routes created for thought & user files

---
+++ models/thought.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {type: String, required: true, minLength:1, maxLength:280},
+        createdAt:{ type: Date, default: Date.now()},
+        username:{type: String, required: true},
+        reactions:[Reaction]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length
+})
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought
